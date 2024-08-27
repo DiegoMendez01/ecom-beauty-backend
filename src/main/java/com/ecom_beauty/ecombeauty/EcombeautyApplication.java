f583c0d6153fa27929e60743c54afc52e40f4087
Sergio <sergiorobayorr@gmail.com>
2024-08-26 21:08:18 -0500

    insert categories

--- src/main/java/com/ecom_beauty/ecombeauty/EcombeautyApplication.java
+++ src/main/java/com/ecom_beauty/ecombeauty/EcombeautyApplication.java
@@ -1,7 +1,15 @@
 package com.ecom_beauty.ecombeauty;
 
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+
+import com.ecom_beauty.ecombeauty.categories.Category;
+import com.ecom_beauty.ecombeauty.categories.CategoryRepository;
 
 @SpringBootApplication
 public class EcombeautyApplication {
@@ -10,4 +18,25 @@ public class EcombeautyApplication {
 		SpringApplication.run(EcombeautyApplication.class, args);
 	}
 
+	@Bean
+	@Profile("dev")
+	public CommandLineRunner initDatabase(CategoryRepository categoryRepository) {
+		return args -> {
+			boolean rebuildDb = Boolean.getBoolean("rebuildDb");
+			
+			if (rebuildDb) {
+				System.out.println("Rebuilding database...");
+				categoryRepository.deleteAll();
+			}
+			
+			System.out.println("Initializing database with dummy data...");
+			categoryRepository.saveAll(List.of(
+				new Category("Skincare"),
+				new Category("Cosmetics"),
+				new Category("Fragrance")
+			));
+			
+			System.out.println("Database initialization complete.");
+		};
+	}
 }
